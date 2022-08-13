8d9348d28a85423211966a7352f1d8b42edeae4e
xrdesignerjohn <xrdesignerjohn@gmail.com>
2022-08-13 16:19:05 -0400

    Redesign, added new assets, Locomotion System

---
+++ ConstructionVR_2_1/Assets/_Assets/Construction Site/Scripts/NightandDay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightandDay : MonoBehaviour {
+
+    //speed at which the sun will rotate
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+
+        //rotate this gameObject to the right (the x-axis) by speed and time
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ ConstructionVR_2_1/Assets/_Assets/Construction Site/Scripts/VideoController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Button playButton, pauseButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnPlayButtonClicked();
+    }
+
+    private void OnEnable()
+    {
+        if(!playButton || !pauseButton)
+        {
+            Debug.LogError("Missing Button Refs on VideoController");
+            return;
+        }
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+        pauseButton.onClick.AddListener(OnPauseButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        playButton.onClick.RemoveListener(OnPlayButtonClicked);
+        pauseButton.onClick.RemoveListener(OnPauseButtonClicked);
+    }
+
+    void OnPlayButtonClicked()
+    {
+        videoPlayer.Play();
+        playButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(true);
+    }
+
+    void OnPauseButtonClicked()
+    {
+        videoPlayer.Pause();
+        playButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(false);
+    }
+}
